7873cd1088451568104eab48c9a76a43e4c49b3a
Darrell Mathews <smathewsbd@gmail.com>
2019-05-21 15:09:20 -0400

    removed JS from HTML  and created separate JS file

---
+++ drumLogic.js
@@ -0,0 +1,22 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key ="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key ="${e.keyCode}"]`);
+    if (!audio) return; // function will stop
+
+    key.classList.add("playing");
+    audio.currentTime = 0; // will rewind audio clip to the beginning of clip
+    audio.play();
+  }
+
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return; // will skip if not a transform property
+    e.target.classList.remove("playing");
+  }
+
+  const keys = Array.from(document.querySelectorAll(".key"));
+  keys.forEach(key =>
+    key.addEventListener('transitionend', removeTransition)
+  );
+  console.table(keys);
+
+  window.addEventListener("keydown", playSound);
